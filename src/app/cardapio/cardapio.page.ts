c7875250bc0ee6c5bda97ecc874d20636301eb0b
Caio1213 <caiofxpgm@gmail.com>
2025-05-15 11:35:43 -0300

    feat: create cardapio page

---
+++ src/app/cardapio/cardapio.page.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cardapio',
+  templateUrl: './cardapio.page.html',
+  styleUrls: ['./cardapio.page.scss'],
+  standalone: false
+})
+export class CardapioPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
