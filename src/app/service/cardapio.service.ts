184c20d2a4250825ec1c3faad4633d597b333826
Caio1213 <136186676+caio1213pgm@users.noreply.github.com>
2025-05-21 11:44:02 -0300

    Update cardapio.service.ts - add chave q faltou

--- src/app/service/cardapio.service.ts
+++ src/app/service/cardapio.service.ts
@@ -30,8 +30,8 @@ export class CardapioService {
 
   getBebidasPorCopo(tipoCopo: string): Observable<DrinkResponse> {
     return this.http.get<DrinkResponse>(`${this.apiBase}/filter.php?g=${tipoCopo}`);
-
-
+  }
+  
   getBebidasPorAlcool(alcoolico: string): Observable<DrinkResponse> {
     return this.http.get<DrinkResponse>(`${this.apiBase}/filter.php?a=${alcoolico}`);
   }
